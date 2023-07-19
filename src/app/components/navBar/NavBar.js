ff8c62704d5bdb0b746ef5fa7f0d6aeb71c5e7fd
Kevin Figueredo <74889406+kevinfigueredo2000@users.noreply.github.com>
2023-07-19 13:58:05 -0300

    cambios NavBar

--- src/app/components/navBar/NavBar.js
+++ src/app/components/navBar/NavBar.js
@@ -4,37 +4,46 @@ import { useRef, useState } from "react";
 import Image from "next/image";
 
 export const Navbar = () => {
-    const [display, setDisplay] = useState("d-none");
-    const [ubicacionPrincipal, setUbicacionPrincipal] = useState(window?.pageYOffset);
-    const [desplazamiento_Actual, setDesplazamiento_Actual] = useState(0);
-    const BgNav = useRef()
-    const Logo = useRef()
-    const Iso = useRef()
-    const ToTop = useRef()
-
-    window?.onscroll = function () {
-        setDesplazamiento_Actual(window?.pageYOffset)
-        if (desplazamiento_Actual >= 380) {
-            BgNav.current?.classList.add("color-nav")
-            BgNav.current?.classList.remove("transparente-nav")
-            Logo.current?.classList.add("d-show")
-            Logo.current?.classList.remove("d-none")
-            Iso.current?.classList.add("d-none")
-            Iso.current?.classList.remove("d-show")
-            ToTop.current?.classList.add("d-show")
-            ToTop.current?.classList.remove("d-none")
-        } else {
-            BgNav.current?.classList.remove("color-nav")
-            BgNav.current?.classList.add("transparente-nav")
-            Logo.current?.classList.add("d-none")
-            Logo.current?.classList.remove("d-show")
-            Iso.current?.classList.add("d-show")
-            Iso.current?.classList.remove("d-none")
-            ToTop.current?.classList.add("d-none")
-            ToTop.current?.classList.remove("d-show")
-        }
-        setUbicacionPrincipal(desplazamiento_Actual);
-    }
+    export const Navbar = () => {
+        const [display, setDisplay] = useState("d-none");
+        const [ubicacionPrincipal, setUbicacionPrincipal] = useState(window?.pageYOffset);
+        const [desplazamiento_Actual, setDesplazamiento_Actual] = useState(0);
+        const BgNav = useRef();
+        const Logo = useRef();
+        const Iso = useRef();
+        const ToTop = useRef();
+      
+        useEffect(() => {
+          const handleScroll = () => {
+            setDesplazamiento_Actual(window?.pageYOffset);
+            if (desplazamiento_Actual >= 380) {
+              BgNav.current?.classList.add("color-nav");
+              BgNav.current?.classList.remove("transparente-nav");
+              Logo.current?.classList.add("d-show");
+              Logo.current?.classList.remove("d-none");
+              Iso.current?.classList.add("d-none");
+              Iso.current?.classList.remove("d-show");
+              ToTop.current?.classList.add("d-show");
+              ToTop.current?.classList.remove("d-none");
+            } else {
+              BgNav.current?.classList.remove("color-nav");
+              BgNav.current?.classList.add("transparente-nav");
+              Logo.current?.classList.add("d-none");
+              Logo.current?.classList.remove("d-show");
+              Iso.current?.classList.add("d-show");
+              Iso.current?.classList.remove("d-none");
+              ToTop.current?.classList.add("d-none");
+              ToTop.current?.classList.remove("d-show");
+            }
+            setUbicacionPrincipal(desplazamiento_Actual);
+          };
+      
+          window?.addEventListener("scroll", handleScroll);
+      
+          return () => {
+            window?.removeEventListener("scroll", handleScroll);
+          };
+        }, [desplazamiento_Actual]);
 
     const handleShow = () => {
         display === "d-none" ? setDisplay("d-block") : setDisplay("d-none")
